ec055bc4b72a38927eb2b2e66e8d8b08d2b26da4
My <my.brindesjo@gmail.com>
2025-02-13 12:40:52 +0100

    New changes 13-02-2025

--- TodoApp/src/pages/Home.jsx
+++ TodoApp/src/pages/Home.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='text-container'>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of the Todo App.</p>
+      <h1>Välkommen till startsidan</h1>
+      <p>Detta är startsidan för Todo-appen.</p>
+      {/* New section added */}
+      <section className='features'>
+        <h2>Funktioner</h2>
+        <ul>
+          <li>Skapa och hantera dina uppgifter</li>
+          <li>Sätt deadlines och påminnelser</li>
+          <li>Organisera uppgifter efter kategorier</li>
+        </ul>
+      </section>
     </div>
   );
 }
